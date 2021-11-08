5889f5ddf118b49e5a23c1e6c79b4cdb9f5b194e
DeppL <302532006@qq.com>
2021-11-08 14:05:56 +0800

    samplex noise

--- Assets/Hashing/Scripts/NoiseVisualization.cs
+++ Assets/Hashing/Scripts/NoiseVisualization.cs
@@ -10,6 +10,8 @@ public class NoiseVisualization : Visualization
     public enum NoiseType {
         Perlin, PerlingTurbulence,
         Value, ValueTurbulence,
+        Simplex, SimplexTurbulence,
+        SimplexValue, SimplexValueTurbulence,
         VoronoiWorleyF1, VoronoiWorleyF2, VoronoiWorleyF2MinusF1,
         VoronoiChebyshevF1, VoronoiChebyshevF2, VoronoiChebyshevF2MinusF1
     }
@@ -72,6 +74,38 @@ public class NoiseVisualization : Visualization
             Job<Lattice2D<LatticeTiling, Turbulence<Value>>>.ScheduleParallel,
             Job<Lattice3D<LatticeNormal, Turbulence<Value>>>.ScheduleParallel,
             Job<Lattice3D<LatticeTiling, Turbulence<Value>>>.ScheduleParallel,
+        },
+        {
+            Job<Simplex1D<Simplex>>.ScheduleParallel,
+            Job<Simplex1D<Simplex>>.ScheduleParallel,
+            Job<Simplex2D<Simplex>>.ScheduleParallel,
+            Job<Simplex2D<Simplex>>.ScheduleParallel,
+            Job<Simplex3D<Simplex>>.ScheduleParallel,
+            Job<Simplex3D<Simplex>>.ScheduleParallel,
+        },
+        {
+            Job<Simplex1D<Turbulence<Simplex>>>.ScheduleParallel,
+            Job<Simplex1D<Turbulence<Simplex>>>.ScheduleParallel,
+            Job<Simplex2D<Turbulence<Simplex>>>.ScheduleParallel,
+            Job<Simplex2D<Turbulence<Simplex>>>.ScheduleParallel,
+            Job<Simplex3D<Turbulence<Simplex>>>.ScheduleParallel,
+            Job<Simplex3D<Turbulence<Simplex>>>.ScheduleParallel,
+        },
+        {
+            Job<Simplex1D<Value>>.ScheduleParallel,
+            Job<Simplex1D<Value>>.ScheduleParallel,
+            Job<Simplex2D<Value>>.ScheduleParallel,
+            Job<Simplex2D<Value>>.ScheduleParallel,
+            Job<Simplex3D<Value>>.ScheduleParallel,
+            Job<Simplex3D<Value>>.ScheduleParallel,
+        },
+        {
+            Job<Simplex1D<Turbulence<Value>>>.ScheduleParallel,
+            Job<Simplex1D<Turbulence<Value>>>.ScheduleParallel,
+            Job<Simplex2D<Turbulence<Value>>>.ScheduleParallel,
+            Job<Simplex2D<Turbulence<Value>>>.ScheduleParallel,
+            Job<Simplex3D<Turbulence<Value>>>.ScheduleParallel,
+            Job<Simplex3D<Turbulence<Value>>>.ScheduleParallel,
         },
         {
             Job<Voronoi1D<LatticeNormal, Worley, F1>>.ScheduleParallel,
